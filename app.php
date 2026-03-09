0afb83b77f4c18bf6456ec1b647cfc33bb40bb0f
DarkieDonno <skstamp@clemson.edu>
2026-03-09 18:10:24 -0400

    Allow CORS from client site

--- app.php
+++ app.php
@@ -1,4 +1,13 @@
 <?php
+header("Access-Control-Allow-Origin: https://projectwarship.netlify.app/");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, X-Requested-With, X-Test-Password");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+
 header("Content-Type: application/json");
 
 /* ===========================
